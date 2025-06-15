d7512a0af2b6733caaee72bc91cf1ceb7c0a0e82
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-06-15 19:11:27 +0700

    Add base application with home view, templates, and routing

--- base/views.py
+++ base/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    """
+    Home page view - displays the hello world landing page
+    """
+    context = {
+        'title': 'Hello World',
+        'message': 'Welcome to Django Social!'
+    }
+    return render(request, 'base/home.html', context)
