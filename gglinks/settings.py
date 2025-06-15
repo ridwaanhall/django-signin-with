49ba54c24fafa8bde0bafa7e4292366e44c786ac
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-06-15 20:48:34 +0700

    Update settings to use environment variables for SECRET_KEY and DEBUG

--- gglinks/settings.py
+++ gglinks/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w#ver92h_jqyel#-mhda_9zrsf8$zhrcvk8r#71z!w)1@nag*3'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
